e4d23502e7a5c90a6c8c28f4ec2c8c4ea321b414
Vinicius Moreira <vinicius_moreira06@hotmail.com>
2019-10-14 11:22:40 -0300

    Esqueci a senha

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.nelioalves.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
